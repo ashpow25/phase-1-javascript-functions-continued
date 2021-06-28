7146cff01edc488383d289b89fd70a9633582510
Ashley <ashpow25@gmail.com>
2021-06-27 17:17:12 -0700

    commit tree 27820fba29f30a54b1213163b41ebbc8f4a681f0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+// Your code here //
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork (work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+mondayWork()
+
+function wrapAdjective(string="*"){
+    return function(parameter="special"){
+        return `You are ${string}${parameter}${string}!`
+    }
+}
